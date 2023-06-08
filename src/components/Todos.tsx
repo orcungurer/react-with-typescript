0c755ef1972d33c8c76668fa8b21901bcb97251a
orcungurer <orcungurer@hotmail.com>
2023-06-08 13:41:27 +0300

    created TodoItem to outsource li item from Todos.

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,11 +1,13 @@
 import Todo from "../models/todo"; // 1
+import TodoItem from "./TodoItem";
 
 // 1
 const Todos: React.FC<{ items: Todo[] }> = (props) => {
   return (
     <ul>
       {props.items.map((item) => (
-        <li key={item.id}>{item.text}</li> // 2
+        // <li key={item.id}>{item.text}</li> // 2
+        <TodoItem key={item.id} text={item.text} /> // 3
       ))}
     </ul>
   );
