47d440f596a1cc85d024b9480425f206f69792dd
orcungurer <orcungurer@hotmail.com>
2023-06-12 20:59:56 +0300

    applied ref for our todo text input.

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,12 +1,24 @@
+import { useRef } from "react"; // 1
+
 const NewTodo = () => {
+  const todoTextInputRef = useRef<HTMLInputElement>(null); // 1, 2
+  
   const submitHandler = (event: React.FormEvent) => {
     event.preventDefault();
+
+    const enteredText = todoTextInputRef.current!.value; // 3
+
+    // 4
+    if (enteredText.trim().length === 0) {
+      return;
+    }
   };
 
   return (
     <form onSubmit={submitHandler}>
       <label htmlFor="text">New Todo</label>
-      <input id="text" type="text" />
+      {/* 1 */}
+      <input id="text" type="text" ref={todoTextInputRef} />
       <button>Submit</button>
     </form>
   );
